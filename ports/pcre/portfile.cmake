40eb30e261462662529a8e231f5dd63f23acfd49
Dale Stammen <dalestam@microsoft.com>
2017-04-08 12:09:16 -0700

    remove ITERATOR_DEBUG_LEVEL

--- ports/pcre/portfile.cmake
+++ ports/pcre/portfile.cmake
@@ -20,7 +20,6 @@ set(VCPKG_LIBRARY_LINKAGE static)
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES ${CMAKE_CURRENT_LIST_DIR}/0001-Fix-uwp.patch
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/0002-Kodi-debug-build-defines.patch
 )
 
 vcpkg_configure_cmake(
